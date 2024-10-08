c01cf7b7989271610206f58d3634913e64a655ba
Gibbled <lucksack@gmail.com>
2024-10-09 01:08:00 +0200

    move all the things that are specific to a certain machine in to the modules/machines subdirectory

--- configuration.nix
+++ configuration.nix
@@ -7,44 +7,16 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
       <home-manager/nixos>
       ./modules/users/ranjit.nix
       ./modules/system/locale.nix
       ./modules/system/packages.nix
       ./modules/system/ssh.nix
       ./modules/system/neovim.nix
+      ./modules/machines/ax8pro/hardware.nix
+      ./modules/machines/ax8pro/hardware-configuration.nix
     ];
 
-  # Bootloader.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.tmp.useTmpfs = true;
-  boot.tmp.tmpfsSize = "12G";
-
-  boot.initrd.luks.devices."luks-16dd3b28-2ac7-4ea3-a866-e8b6939020f5".device = "/dev/disk/by-uuid/16dd3b28-2ac7-4ea3-a866-e8b6939020f5";
-  networking.hostName = "xybr"; # Define your hostname.
-  boot.kernelParams = [
-  "video=Card1-DP-1:1920x1080@60"
-  "video=Card1-DP-2:1920x1080@60"
-  "video=Card1-HDMI-1:1920x1080@60"
-  "video=Card1-HDMI-2:1920x1080@60"
-  "kernel.dmesg_restrict=0"
-];
-  systemd.tmpfiles.rules = 
-  let
-    rocmEnv = pkgs.symlinkJoin {
-      name = "rocm-combined";
-      paths = with pkgs.rocmPackages; [
-        rocblas
-        hipblas
-        clr
-      ];
-    };
-  in [
-    "L+    /opt/rocm   -    -    -     -    ${rocmEnv}"
-  ];
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
@@ -97,18 +69,6 @@
 #Allow the wheel group to not use a password for sudo
 security.sudo.wheelNeedsPassword = false;
 
-
-  #All the AMDgpu stuff
-  #hardware.opengl = {
-  #driSupport = true;
-  #driSupport32Bit = true;
-  #};
-  systemd.packages = with pkgs; [ lact ];
-  systemd.services.lactd.wantedBy = ["multi-user.target"];
-  hardware.graphics.extraPackages = with pkgs; [
-  rocmPackages.clr.icd
-  pkgs.amdvlk
-];
 
 #This is for Remote Desktop config
 
---
+++ modules/machines/ax8pro/hardware.nix
@@ -0,0 +1,49 @@
+{ config, pkgs, ...}:
+
+{
+
+  # Bootloader.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.tmp.useTmpfs = true;
+  boot.tmp.tmpfsSize = "12G";
+
+  boot.initrd.luks.devices."luks-16dd3b28-2ac7-4ea3-a866-e8b6939020f5".device = "/dev/disk/by-uuid/16dd3b28-2ac7-4ea3-a866-e8b6939020f5";
+  networking.hostName = "xybr"; # Define your hostname.
+  boot.kernelParams = [
+  "video=Card1-DP-1:1920x1080@60"
+  "video=Card1-DP-2:1920x1080@60"
+  "video=Card1-HDMI-1:1920x1080@60"
+  "video=Card1-HDMI-2:1920x1080@60"
+  "kernel.dmesg_restrict=0"
+];
+  systemd.tmpfiles.rules =
+  let
+    rocmEnv = pkgs.symlinkJoin {
+      name = "rocm-combined";
+      paths = with pkgs.rocmPackages; [
+        rocblas
+        hipblas
+        clr
+      ];
+    };
+  in [
+    "L+    /opt/rocm   -    -    -     -    ${rocmEnv}"
+  ];
+
+
+
+  #All the AMDgpu stuff
+  #hardware.opengl = {
+  #driSupport = true;
+  #driSupport32Bit = true;
+  #};
+  systemd.packages = with pkgs; [ lact ];
+  systemd.services.lactd.wantedBy = ["multi-user.target"];
+  hardware.graphics.extraPackages = with pkgs; [
+  rocmPackages.clr.icd
+  pkgs.amdvlk
+];
+
+}
