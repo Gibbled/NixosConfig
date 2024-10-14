95f6d72f58c583c307193a3658e7937a037657c8
Gibbled <lucksack@gmail.com>
2024-10-14 17:10:30 +0200

    backup to the working config

--- flake.nix
+++ flake.nix
@@ -1,3 +1,4 @@
+
 {
   description = "main flake";
 
@@ -14,11 +15,56 @@
 
   outputs = { nixpkgs, home-manager, ... }@inputs:
 {
-        modules = [ 
+        modules = [
         ./configuration.nix
-	./flakes/home.nix 
-	];
+        ./flakes/home.nix
+        ];
 
 
 };
 }
+ranjit in 🌐 xybr in Nixos on  master ❯ git switch gibbler
+Switched to branch 'gibbler'
+ranjit in 🌐 xybr in Nixos on  gibbler ❯ cat flake.nix
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # home-manager, used for managing user configuration
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      # TODO please change the hostname to your own
+      xybr = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+
+          # make home-manager as a module of nixos
+          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            # TODO replace ryan with your own username
+            home-manager.users.ranjit = import ./flakes/home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
+        ];
+      };
+    };
+  };
+}
+
