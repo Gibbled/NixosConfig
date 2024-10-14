0d1beb7b86b64e9310ef448ca92f4984a4582340
Gibbled <lucksack@gmail.com>
2024-10-14 15:44:20 +0200

    keeps all the flakes in the flakes directory

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
             home-manager.useUserPackages = true;
 
             # TODO replace ryan with your own username
-            home-manager.users.ranjit = import ./home.nix;
+            home-manager.users.ranjit = import ./flakes/home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
           }
