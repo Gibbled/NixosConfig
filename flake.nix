89b40cb9d7798c0c52b587253fe6fefaa7793182
Gibbled <lucksack@gmail.com>
2024-10-14 16:10:52 +0200

    try again

--- flake.nix
+++ flake.nix
@@ -1,21 +1,18 @@
 {
-  description = "NixOS configuration";
+  description = "main flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # home-manager, used for managing user configuration
     home-manager = {
-      url = "github:nix-community/home-manager/master";
-      # The `follows` keyword in inputs is used for inheritance.
-      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
-      # the `inputs.nixpkgs` of the current flake,
-      # to avoid problems caused by different versions of nixpkgs.
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nix-colors.url = "github:misterio77/nix-colors";
+
   };
 
- outputs = { nixpkgs, home-manager, ... }@inputs:
+  outputs = { nixpkgs, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -26,6 +23,7 @@
         extraSpecialArgs = { inherit inputs; };
 
         modules = [ ./flakes/home.nix ];
+
       };
     };
 
