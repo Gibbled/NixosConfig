ef607960d8fc13cefca81801b03883e375a1efce
Gibbled <lucksack@gmail.com>
2024-10-14 20:11:59 +0200

    another try at nix-colors

--- flakes/nix-colors.nix
+++ flakes/nix-colors.nix
@@ -1,16 +1,11 @@
+{ pkgs, config, ...}:
+
 {
 
-  inputs = {
-    nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
+inputs = {
 
-    # Upstream source of .yaml base16 schemes
-    base16-schemes.url = "github:tinted-theming/base16-schemes";
-    base16-schemes.flake = false;
-  };
+nix-colors.url = "github:misterio77/nix-colors"
 
-  outputs = { self, nixpkgs-lib, base16-schemes }:
-    import ./. {
-      nixpkgs-lib = nixpkgs-lib.lib;
-      base16-schemes = base16-schemes.outPath;
-    };
-}	
+};
+
+}
