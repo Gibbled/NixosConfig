e4d42d3cd1ba1fe74c40eb90ff7d5020570677b1
Gibbled <lucksack@gmail.com>
2024-10-14 14:47:49 +0200

    more homemanager things...

--- flake.nix
+++ flake.nix
@@ -1,12 +1,11 @@
 {
-  description = "Top level NixOS flake";
+  description = "NixOS configuration";
 
   inputs = {
-    # NixOS official package source, using the nixos-23.11 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-     # home-manager, used for managing user configuration
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # home-manager, used for managing user configuration
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-unstable";
       # The `follows` keyword in inputs is used for inheritance.
       # Here, `inputs.nixpkgs` of home-manager is kept consistent with
       # the `inputs.nixpkgs` of the current flake,
@@ -15,16 +14,15 @@
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    # Please replace my-nixos with your hostname
-    nixosConfigurations.xybr = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./configuration.nix
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      # TODO please change the hostname to your own
+      xybr = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
 
-	# make home-manager as a module of nixos
+          # make home-manager as a module of nixos
           # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
           home-manager.nixosModules.home-manager
           {
@@ -36,7 +34,8 @@
 
             # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
           }
-      ];
+        ];
+      };
     };
   };
 }
