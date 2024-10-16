675b50ed1ae9924103723292c28faf698fa23e4e
Gibbled <lucksack@gmail.com>
2024-10-16 17:44:27 +0200

    this didn't work, more reading required

--- flakes/home-manager/home.nix
+++ flakes/home-manager/home.nix
@@ -77,24 +77,6 @@
       aws.disabled = true;
       gcloud.disabled = true;
       line_break.disabled = true;
-      format = """
-[░▒▓](#a3aed2)\
-[  ](bg:#a3aed2 fg:#090c0c)\
-[](bg:#769ff0 fg:#a3aed2)\
-$directory\
-[](fg:#769ff0 bg:#394260)\
-$git_branch\
-$git_status\
-[](fg:#394260 bg:#212736)\
-$nodejs\
-$rust\
-$golang\
-$php\
-[](fg:#212736 bg:#1d2230)\
-$time\
-[ ](fg:#1d2230)\
-\n$character""";
-
     };
   };
 
