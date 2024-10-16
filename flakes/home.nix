62892a7ee9c105f43137f6b6c870ed9d6f1a015b
Gibbled <lucksack@gmail.com>
2024-10-16 12:34:20 +0200

    made some cleanup and added option to enable bash integration in starship

--- flakes/home.nix
+++ flakes/home.nix
@@ -1,53 +1,29 @@
 { config, pkgs,  ... }:
 
 {
-  # TODO please change the username & home directory to your own
   home.username = "ranjit";
   home.homeDirectory = "/home/ranjit";
   imports = [
   #  inputs.nix-colors.homeManagerModules.default
   ];
 
-  # link the configuration file in current directory to the specified location in home directory
-  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
-
-  # link all files in `./scripts` to `~/.config/i3/scripts`
-  # home.file.".config/i3/scripts" = {
-  #   source = ./scripts;
-  #   recursive = true;   # link recursively
-  #   executable = true;  # make all files executable
-  # };
-
-  # encode the file content in nix configuration file directly
-  # home.file.".xxx".text = ''
-  #     xxx
-  # '';
-
-  # set cursor size and dpi for 4k monitor
   xresources.properties = {
-    "Xcursor.size" = 16;
+    "Xcursor.size" = 20;
     "Xft.dpi" = 172;
   };
 
-  # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
-    # here is some command line tools I use frequently
-    # feel free to add your own or remove some of them
-
     neofetch
-
     # archives
     zip
     xz
     unzip
     p7zip
-
     # utils
     ripgrep # recursively searches directories for a regex pattern
     jq # A lightweight and flexible command-line JSON processor
     yq-go # yaml processor https://github.com/mikefarah/yq
     fzf # A command-line fuzzy finder
-
     # networking tools
     mtr # A network diagnostic tool
     iperf3
@@ -57,7 +33,6 @@
     socat # replacement of openbsd-netcat
     nmap # A utility for network discovery and security auditing
     ipcalc  # it is a calculator for the IPv4/v6 addresses
-
     # misc
     file
     which
@@ -68,22 +43,15 @@
     zstd
     gnupg
     # nix related
-    #
-    # it provides the command `nom` works just like `nix`
-    # with more details log output
     nix-output-monitor
-
     # productivity
-
     btop  # replacement of htop/nmon
     iotop # io monitoring
     iftop # network monitoring
-
     # system call monitoring
     strace # system call monitoring
     ltrace # library call monitoring
     lsof # list open files
-
     # system tools
     sysstat
     lm_sensors # for `sensors` command
@@ -104,7 +72,8 @@
     enable = true;
     # custom settings
     settings = {
-      add_newline = false;
+      enableBashIntegration = true;
+      add_newline = true;
       aws.disabled = true;
       gcloud.disabled = true;
       line_break.disabled = true;
