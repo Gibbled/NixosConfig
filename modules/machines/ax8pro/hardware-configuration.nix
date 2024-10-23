c9ccb96197dd1c8cf9fc748077d5926db3d3fe7c
Gibbled <lucksack@gmail.com>
2024-10-23 19:59:32 +0200

    users.users

--- modules/machines/ax8pro/hardware-configuration.nix
+++ modules/machines/ax8pro/hardware-configuration.nix
@@ -42,7 +42,7 @@
         enable = true;
         port = 22;
         hostKeys = [ "/etc/ssh/ssh_host_ed25519_key" ];
-        authorizedKeys = users.ranjit.openssh.authorizedKeys.keys;
+        authorizedKeys = users.users.ranjit.openssh.authorizedKeys.keys;
       };
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
