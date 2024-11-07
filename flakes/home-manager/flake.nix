9ac4280971a440bc7f38fd1247be0095747da97c
Gibbled <lucksack@gmail.com>
2024-11-07 20:20:57 +0100

    gg

--- flakes/home-manager/flake.nix
+++ flakes/home-manager/flake.nix
@@ -1,15 +1,10 @@
-{
-  description = "A very basic flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  { pkgs, config, ... }:
 
-  outputs = { self, nixpkgs }: {
+  { self, nixpkgs }: {
 
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
-  };
 }
