0404615317337209ceaa820975fe9b60af614447
ranjit pindi <lucksack@gmail.com>
2025-03-02 21:26:07 +0100

    removed a bunch of stuff from configuration.nix, next get it all modularized and into the flake.

--- configuration.nix
+++ configuration.nix
@@ -4,24 +4,7 @@
   imports =
     [ 
       ./modules/users/ranjit.nix
-      ./modules/system/locale.nix
-      ./modules/system/ollama.nix
-      ./modules/system/virt-manager.nix
-      ./modules/system/searxng.nix
-      ./modules/system/privoxy.nix
-      ./modules/system/tor-browser.nix
-      ./modules/system/packages.nix
-      ./modules/system/ssh.nix
-      ./modules/system/neovim.nix
-      ./modules/system/suid.nix
-      ./modules/system/docker.nix
-      ./modules/system/flakes.nix
-      ./modules/system/firewall.nix
-      ./modules/system/proxy.nix
-      ./modules/system/ldfix.nix
-      #./modules/system/firefox.nix
-      ./modules/system/desktop-environment.nix
-      ./modules/system/remote-desktop.nix
+      ./modules/system
       ./modules/machines/ax8pro/hardware.nix
       ./hardware-configuration.nix
     ];
---
+++ modules/system/default.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, specialArgs, ... }:
+
+{   
+
+imports = [
+
+./desktop-environment.nix
+./docker.nix
+./firefox.nix
+./firewall.nix
+./flakes.nix
+./ldfix.nix
+./locale.nix
+./neovim.nix
+./ollama.nix
+./packages.nix
+./privoxy.nix
+./proxy.nix
+./remote-desktop.nix
+./searxng.nix
+./ssh.nix
+./suid.nix
+./tor-browser.nix
+./virt-manager.nix
+
+];
+
+}
