96603b7d89a418ea7f3e9c0ab4cc8ceb17da98de
Gibbled <lucksack@gmail.com>
2025-06-13 10:32:23 +0200

    modification GDK scale set to 2 because all the gtk applications have obnoxiously small fonts

--- flakes/home-manager/bash.nix
+++ flakes/home-manager/bash.nix
@@ -10,16 +10,15 @@
       export LESS="-X"
       PAGER=less LESS=R
       clear && `which neofetch`
+      export GDK_DPI_SCALE="2"
       '';
 
       shellAliases = {
       vi = "nvim";
       gc = "git commit -a -m $1";
-      sgp = "sudo git pull";
       upd = "sudo nix-channel --update && sudo nix flake update";
       rebuild = "sudo nixos-rebuild switch --upgrade |& nom";
       cleanup = "nix-collect-garbage --delete-old && sudo nix-collect-garbage --delete-old";
-      #dmesg = "sudo dmesg";
       epoch = "echo \"obase\=2;`date '+%s'`\" | bc";
 
       };
