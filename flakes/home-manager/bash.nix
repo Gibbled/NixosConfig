a7504a0b0e4e04c1aa01766f45a5cd9ddcd9e452
Gibbled <lucksack@gmail.com>
2025-06-17 12:40:52 +0200

    added gnr and grr aliases

--- flakes/home-manager/bash.nix
+++ flakes/home-manager/bash.nix
@@ -20,6 +20,8 @@
       rebuild = "sudo nixos-rebuild switch --upgrade |& nom";
       cleanup = "nix-collect-garbage --delete-old && sudo nix-collect-garbage --delete-old";
       epoch = "echo \"obase\=2;`date '+%s'`\" | bc";
+      gnr = "ssh -i /home/ranjit/.ssh/id_noringu_ecdsa -p 2525 'ranjit@gragnet.dyndns.org'";
+      grr = "ssh -i /home/ranjit/.ssh/id_ringu_ecdsa -p 2525 'ranjit@gragnet.dyndns.org'";
 
       };
     };
