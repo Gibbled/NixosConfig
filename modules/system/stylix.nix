ec804f7280add6cbfc6001b7aab5178f2628e372
Gibbled <lucksack@gmail.com>
2025-07-10 23:36:39 +0200

    stylix clobbers nvim/init.lua

--- modules/system/stylix.nix
+++ modules/system/stylix.nix
@@ -1,8 +1,9 @@
-{ pkgs, ...}:
+{ pkgs,  ...}:
 
 {
 
   stylix.enable = true;
+  #stylix.targets.neovim.enable = false;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml"; 
 
 }
