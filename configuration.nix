3d31075c7d76adcc47d671b2b7fe1a7cd5145805
Gibbled <lucksack@gmail.com>
2025-07-12 00:07:22 +0200

    cleanup

--- configuration.nix
+++ configuration.nix
@@ -3,13 +3,9 @@
 {
   imports =
     [ 
-      #./modules/users/ranjit.nix
-      #./modules/system
-      #./modules/machines/ax8pro/hardware.nix
-      #./hardware-configuration.nix
       #It's all in the flake now.
     ];
 
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "24.05"; # Did you read the comment? Yep!
 
 }
