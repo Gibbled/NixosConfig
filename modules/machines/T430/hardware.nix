7719c949361da5879d93bb5fe0726f0884d625dc
Gibbled <lucksack@gmail.com>
2025-07-22 23:05:17 +0200

    pretty on the other machine with plymouth

--- modules/machines/T430/hardware.nix
+++ modules/machines/T430/hardware.nix
@@ -2,9 +2,45 @@
 
 {
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+boot = {
+  kernelPackages = pkgs.linuxPackages_latest;
+  loader.systemd-boot.enable = true;
+  loader.efi.canTouchEfiVariables = true;
+  
+      plymouth = {
+        enable = true;
+        theme = "abstract_ring";
+        themePackages = with pkgs; [
+          # By default we would install all themes
+          (adi1090x-plymouth-themes.override {
+            selected_themes = [
+            "rings"
+            "abstract_ring"
+            "cyanide"
+            "glitch"
+            "glowing"
+            ];
+          })
+        ];
+      };
+
+      # Enable "Silent boot"
+      consoleLogLevel = 3;
+      initrd.verbose = false;
+      kernelParams = [
+        "quiet"
+        "splash"
+        "boot.shell_on_fail"
+        "udev.log_priority=3"
+        "rd.systemd.show_status=auto"
+      ];
+      # Hide the OS choice for bootloaders.
+      # It's still possible to open the bootloader list by pressing any key
+      # It will just not appear on screen unless a key is pressed
+      loader.timeout = 0;
+      };
+
+
 
   networking.hostName = "stinkpad"; # Define your hostname.
   hardware.keyboard.qmk.enable = true;
