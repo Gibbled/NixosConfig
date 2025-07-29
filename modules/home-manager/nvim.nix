20323c99422096ebb209c13f9c4dc2ae18212c58
Gibbled <lucksack@gmail.com>
2025-07-29 14:51:51 +0200

    vim config updated with alpha-nvim

--- modules/home-manager/nvim.nix
+++ modules/home-manager/nvim.nix
@@ -43,6 +43,7 @@ in
 	  #snacks-nvim
 	  noice-nvim
 	  nvim-notify
+	  alpha-nvim
       ];
        extraLuaConfig = ''
 
@@ -93,6 +94,51 @@ in
 
 	 --Lualine
 	 require('lualine').setup()
+
+	 --Alpha
+	 local alpha = require('alpha')
+	 local status_ok, alpha = pcall(require, "alpha")
+         if not status_ok then
+          return
+         end
+         
+         local dashboard = require("alpha.themes.dashboard")
+         dashboard.section.header.val = {
+
+             [[          ▀████▀▄▄              ▄█ ]],
+             [[            █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█ ]],
+             [[    ▄        █          ▀▀▀▀▄  ▄▀  ]],
+             [[   ▄▀ ▀▄      ▀▄              ▀▄▀  ]],
+             [[  ▄▀    █     █▀   ▄█▀▄      ▄█    ]],
+             [[  ▀▄     ▀▄  █     ▀██▀     ██▄█   ]],
+             [[   ▀▄    ▄▀ █   ▄██▄   ▄  ▄  ▀▀ █  ]],
+             [[    █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀  ]],
+             [[   █   █  █      ▄▄           ▄▀   ]],
+         
+         }
+
+          dashboard.section.buttons.val = {
+            dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+            dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+            dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+            dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+            dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.vim<CR>"),
+            dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+         }
+
+         local function footer()
+          return "The Tao Rewards Good and Punishes Evil"
+         end
+         
+         dashboard.section.footer.val = footer()
+         
+         dashboard.section.footer.opts.hl = "Type"
+         dashboard.section.header.opts.hl = "Include"
+         dashboard.section.buttons.opts.hl = "Keyword"
+         
+         dashboard.opts.opts.noautocmd = true
+         alpha.setup(dashboard.opts)
+         
 
          --Mason Config
 	 local lsp_zero = require('lsp-zero')
