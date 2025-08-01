68cbce022c7ac8822dc45cc3bbb51eeef77d6767
Gibbled <lucksack@gmail.com>
2025-08-01 21:59:08 +0200

    some fixes for stinkpad

--- flake.nix
+++ flake.nix
@@ -88,6 +88,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+	    home-manager.extraSpecialArgs = { inherit inputs; };
 	    home-manager.sharedModules = [ 
             sops-nix.homeManagerModules.sops
 	      ];
