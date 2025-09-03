21a9c7b5115d9bd2e416ca4105d4835c9eefb4f6
Gibbled <lucksack@gmail.com>
2025-09-03 14:45:42 +0200

    upd

--- modules/machines/T430/host-config/wireguard_server.nix
+++ modules/machines/T430/host-config/wireguard_server.nix
@@ -3,7 +3,7 @@
 
   # enable NAT
   networking.nat.enable = true;
-  networking.nat.externalInterface = "eth0";
+  networking.nat.externalInterface = "enp0s25";
   networking.nat.internalInterfaces = [ "wg0" ];
   networking.firewall = {
     allowedUDPPorts = [ 51820 ];
@@ -14,7 +14,8 @@
     # "wg0" is the network interface name. You can name the interface arbitrarily.
     wg0 = {
       # Determines the IP address and subnet of the server's end of the tunnel interface.
-      ips = [ "10.100.0.1/24" ];
+      #ips = [ "10.100.0.1/24" ];
+      ips = [ "172.16.0.1/24" ];
 
       # The port that WireGuard listens to. Must be accessible by the client.
       listenPort = 51820;
@@ -22,12 +23,12 @@
       # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
       # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
       postSetup = ''
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 172.16.0.0/16 -o enp0s25 -j MASQUERADE
       '';
 
       # This undoes the above command
       postShutdown = ''
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 172.16.0.0/16 -o enp0s25 -j MASQUERADE
       '';
 
       # Path to the private key file.
@@ -40,9 +41,9 @@
       peers = [
         # List of allowed peers.
         { 
-          name = "Xybr";
+          name = "xybr";
           publicKey = "/etc/wireguard/keys/xybr-public.txt";
-          allowedIPs = [ "10.100.0.3/32" ];
+          allowedIPs = [ "172.16.0.3/16" ];
         }
       ];
     };
