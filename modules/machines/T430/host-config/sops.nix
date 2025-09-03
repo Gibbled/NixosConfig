bc01cba853d459d30ae95cd10a825c932fd29c98
Gibbled <lucksack@gmail.com>
2025-09-03 12:30:05 +0200

    sops

--- modules/machines/T430/host-config/sops.nix
+++ modules/machines/T430/host-config/sops.nix
@@ -2,20 +2,21 @@
 
 {
 
-
-#sops.secrets.wireguard-gragnet-private = { 
-    #restartUnits = [ "wg-quick-wg0.service" ];
-    #path = "/etc/wireguard/keys/privatekey";
-#};
-#
-#sops.secrets.wireguard-gragnet-public = { 
-    #restartUnits = [ "wg-quick-wg0.service" ];
-    #path = "/etc/wireguard/keys/publickey";
-#};
-
 sops.secrets.ddclient_secret = {
      path = "/home/ranjit/.config/ddclient/secret.txt";
      owner = config.users.users.ranjit.name;
 };
 
+sops.secrets.wireguard-gragnet-public = {
+     restartUnits = [ "wg-quick-wg0.service" ];
+     path = "/etc/wireguard/xybr-public.txt";
+};
+sops.secrets.wireguard-stinkpadPrivate = {
+     restartUnits = [ "wg-quick-wg0.service" ];
+     path = "/etc/wireguard/management-private.txt";
+};
+sops.secrets.wireguard-stinkpadPublic = {
+     restartUnits = [ "wg-quick-wg0.service" ];
+     path = "/etc/wireguard/management-public.txt";
+};
 }
