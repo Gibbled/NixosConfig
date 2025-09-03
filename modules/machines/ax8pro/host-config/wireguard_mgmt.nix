5b15c3c6f8a8439b9fca950002a01611a7f770d8
Gibbled <lucksack@gmail.com>
2025-09-03 13:12:45 +0200

    wg

--- modules/machines/ax8pro/host-config/wireguard_mgmt.nix
+++ modules/machines/ax8pro/host-config/wireguard_mgmt.nix
@@ -1,16 +1,17 @@
-{ inputs, pkgs, sops-nix, ... }: 
-
-
-
 {
+  inputs,
+  pkgs,
+  sops-nix,
+  ...
+}: {
   networking.networkmanager.dns = "systemd-resolved";
   services.resolved.enable = true;
   networking.wg-quick.interfaces = let
-  systemd.services.wg-quick-wg1 = {
-    requires = [ "network-online.target" "NetworkManager.service" ];
-    after = [ "graphical.target" ];
-    wants = [ "graphical.target" ];
-      };
+    systemd.services.wg-quick-wg1 = {
+      requires = ["network-online.target" "NetworkManager.service"];
+      after = ["graphical.target"];
+      wants = ["graphical.target"];
+    };
     server_ip = "zabbix.homeunix.org";
   in {
     wg1 = {
@@ -26,15 +27,14 @@
       # Path to the private key file.
       privateKeyFile = "/etc/wireguard/keys/privatekey";
 
-      peers = [{
-        publicKey = "hY8/uhiXJdGcQPw5Cj8sOtDoM+69pR0gZpYsO5SY0yQ=";
-        allowedIPs = [ "10.100.0.0/8" ];
-        endpoint = "${server_ip}:51820";
-        persistentKeepalive = 25;
-      }];
+      peers = [
+        {
+          publicKey = "hY8/uhiXJdGcQPw5Cj8sOtDoM+69pR0gZpYsO5SY0yQ=";
+          allowedIPs = ["10.100.0.0/8"];
+          endpoint = "${server_ip}:51820";
+          persistentKeepalive = 25;
+        }
+      ];
     };
   };
 }
-
-
-
