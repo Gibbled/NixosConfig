83e376306e2b2238ac304c79497afaaef674ba1f
Gibbled <lucksack@gmail.com>
2025-10-18 12:10:43 -0400

    fix searxng configuration

--- modules/system/searxng.nix
+++ modules/system/searxng.nix
@@ -3,106 +3,204 @@
   lib,
   pkgs,
   ...
-}: let
-  inherit (lib) head mapAttrsToList;
-in {
+}: {
   services.searx = {
     enable = true;
+    redisCreateLocally = true;
 
-    package =
-      (pkgs.searxng.override {
-        ## FIXME: https://github.com/NixOS/nixpkgs/issues/380351
-        #python3 = pkgs.python3.override {
-          #packageOverrides = pyFinal: pyPrev: {
-            #httpx = pyPrev.httpx.overridePythonAttrs (o: rec {
-              #version = "0.27.2";
-              ##src = pkgs.fetchFromGitHub {
-                #owner = "encode";
-                #repo = o.pname;
-                #tag = version;
-                #hash = "sha256-N0ztVA/KMui9kKIovmOfNTwwrdvSimmNkSvvC+3gpck=";
-              #};
-            #});
-            #starlette = pyPrev.starlette.overridePythonAttrs (o: rec {
-              #version = "0.41.2";
-              #src = pkgs.fetchFromGitHub {
-                #owner = "encode";
-                #repo = "starlette";
-                #tag = version;
-                #hash = "sha256-ZNB4OxzJHlsOie3URbUnZywJbqOZIvzxS/aq7YImdQ0=";
-              #};
-            #});
-            #httpx-socks = pyPrev.httpx-socks.overridePythonAttrs (o: rec {
-              #version = "0.9.2";
-              #src = pkgs.fetchFromGitHub {
-                #owner = "romis2012";
-                #repo = "httpx-socks";
-                #tag = "v${version}";
-                #hash = "sha256-PUiciSuDCO4r49st6ye5xPLCyvYMKfZY+yHAkp5j3ZI=";
-              #};
-            #});
-          #};
-        #};
+    package = (pkgs.searxng.override {
       })
       .overrideAttrs (o: {
-        postInstall = ''
-          ${o.postInstall or ""}
-            ## Replace logo
-            cp ${./logo.png} $out/${pkgs.python3.sitePackages}/searx/static/themes/simple/img/searxng.png
-        '';
-      });
+      postInstall = ''
+        ${o.postInstall or ""}
+          ## Replace logo
+          cp ${./logo.png} $out/${pkgs.python3.sitePackages}/searx/static/themes/simple/img/searxng.png
+      '';
+    });
+    # Rate limiting
+    limiterSettings = {
+      real_ip = {
+        x_for = 1;
+        ipv4_prefix = 32;
+        ipv6_prefix = 56;
+      };
 
-    #environmentFile = config.sops.secrets.searxng.path;
+      botdetection = {
+        ip_limit = {
+          filter_link_local = true;
+          link_token = true;
+        };
+      };
+    };
 
+    # UWSGI configuration
+    runInUwsgi = true;
+
+    uwsgiConfig = {
+      socket = "/run/searx/searx.sock";
+      http = ":8888";
+      chmod-socket = "660";
+    };
+
+    # Searx configuration
     settings = {
+      # Instance settings
       general = {
-        instance_name = "GragFind";
         debug = false;
+        instance_name = "SearXNG Instance";
+        donation_url = false;
+        contact_url = false;
+        privacypolicy_url = false;
         enable_metrics = false;
       };
 
-      search = {
-        autocomplete = "google";
-        favicon_resolver = "google";
-
-        safe_search = 0;
-
-        default_lang = "en-CA";
-      };
-
+      # User interface
       ui = {
-        infinite_scroll = true;
+        static_use_hash = true;
+        default_locale = "en";
         query_in_title = true;
+        infinite_scroll = false;
+        center_alignment = true;
+        default_theme = "simple";
+        theme_args.simple_style = "auto";
+        search_on_category_select = false;
         hotkeys = "vim";
       };
 
-      server = {
-        port = 8888;
-        #bind_address = (head config.services.pcsd.virtualIps).ip;
-        bind_address = "127.0.0.1";
-
-        #secret_key = "@SEARXNG_SECRET@";
-        secret_key = "TimmyJimmy";
-
-        public_instance = false;
+      # Search engine settings
+      search = {
+        safe_search = 2;
+        autocomplete_min = 2;
+        autocomplete = "duckduckgo";
+        ban_time_on_fail = 5;
+        max_ban_time_on_fail = 120;
       };
 
-      engines = mapAttrsToList (name: value: {inherit name;} // value) {
-        "duckduckgo".disabled = false;
-        "duckduckgo images".disabled = false;
-        "gitlab".disabled = false;
-        "qwant".disabled = false;
-        "reddit".disabled = false;
+      # Server configuration
+      server = {
+        base_url = "https://search.example.com";
+        port = 8888;
+        bind_address = "127.0.0.1";
+        #secret_key = config.sops.secrets.searx.path;
+        secret_key = "timmyjimmy";
+        limiter = true;
+        public_instance = true;
+        image_proxy = true;
+        method = "GET";
+      };
 
-        #"wikipedia" = {
-          #engine = "wikipedia";
-          #shortcut = "w";
-          #base_url = "https://wikipedia.org/";
-        #};
+      # Search engines
+      engines = lib.mapAttrsToList (name: value: {inherit name;} // value) {
+        "duckduckgo".disabled = true;
+        "brave".disabled = true;
+        "bing".disabled = false;
+        "mojeek".disabled = true;
+        "mwmbl".disabled = false;
+        "mwmbl".weight = 0.4;
+        "qwant".disabled = true;
+        "crowdview".disabled = false;
+        "crowdview".weight = 0.5;
+        "curlie".disabled = true;
+        "ddg definitions".disabled = false;
+        "ddg definitions".weight = 2;
+        "wikibooks".disabled = false;
+        "wikidata".disabled = false;
+        "wikiquote".disabled = true;
+        "wikisource".disabled = true;
+        "wikispecies".disabled = false;
+        "wikispecies".weight = 0.5;
+        "wikiversity".disabled = false;
+        "wikiversity".weight = 0.5;
+        "wikivoyage".disabled = false;
+        "wikivoyage".weight = 0.5;
+        "currency".disabled = true;
+        "dictzone".disabled = true;
+        "lingva".disabled = true;
+        "bing images".disabled = false;
+        "brave.images".disabled = true;
+        "duckduckgo images".disabled = true;
+        "google images".disabled = false;
+        "qwant images".disabled = true;
+        "1x".disabled = true;
+        "artic".disabled = false;
+        "deviantart".disabled = false;
+        "flickr".disabled = true;
+        "imgur".disabled = false;
+        "library of congress".disabled = false;
+        "material icons".disabled = true;
+        "material icons".weight = 0.2;
+        "openverse".disabled = false;
+        "pinterest".disabled = true;
+        "svgrepo".disabled = false;
+        "unsplash".disabled = false;
+        "wallhaven".disabled = false;
+        "wikicommons.images".disabled = false;
+        "yacy images".disabled = true;
+        "bing videos".disabled = false;
+        "brave.videos".disabled = true;
+        "duckduckgo videos".disabled = true;
+        "google videos".disabled = false;
+        "qwant videos".disabled = false;
+        "dailymotion".disabled = true;
+        "google play movies".disabled = true;
+        "invidious".disabled = true;
+        "odysee".disabled = true;
+        "peertube".disabled = false;
+        "piped".disabled = true;
+        "rumble".disabled = false;
+        "sepiasearch".disabled = false;
+        "vimeo".disabled = true;
+        "youtube".disabled = false;
+        "brave.news".disabled = true;
+        "google news".disabled = true;
+      };
 
-        "github" = {
-          engine = "github";
-          shortcut = "gh";
+      # Outgoing requests
+      outgoing = {
+        request_timeout = 5.0;
+        max_request_timeout = 15.0;
+        pool_connections = 100;
+        pool_maxsize = 15;
+        enable_http2 = true;
+      };
+
+      # Enabled plugins
+      enabled_plugins = [
+        "Basic Calculator"
+        "Hash plugin"
+        "Tor check plugin"
+        "Open Access DOI rewrite"
+        "Hostnames plugin"
+        "Unit converter plugin"
+        "Tracker URL remover"
+      ];
+    };
+  };
+
+  # Systemd configuration
+  systemd.services.nginx.serviceConfig.ProtectHome = false;
+
+  # User management
+  users.groups.searx.members = ["nginx"];
+
+  # Nginx configuration
+  services.nginx = {
+    enable = true;
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    virtualHosts = {
+      "search.example.com" = {
+        forceSSL = false;
+        sslCertificate = "...";
+        sslCertificateKey = "...";
+        locations = {
+          "/" = {
+            extraConfig = ''
+              uwsgi_pass unix:${config.services.searx.uwsgiConfig.socket};
+            '';
+          };
         };
       };
     };
