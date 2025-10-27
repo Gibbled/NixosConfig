a1faee301a75404616757bb3bcee33e348e66415
Gibbled <lucksack@gmail.com>
2025-10-27 14:18:27 -0400

    some tweaks to tor

--- modules/system/privoxy.nix
+++ modules/system/privoxy.nix
@@ -1,13 +1,13 @@
-{ config, pkgs, ... }:
-
 {
- 
-services.privoxy = {
-
-enable = true;
-
-
-};
-
-
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  services.privoxy = {
+    enable = true;
+    #inspectHttps = true;
+    enableTor = true;
+    settings.forward-socks5 = lib.mkDefault ".onion localhost:9050 .";
+  };
 }
