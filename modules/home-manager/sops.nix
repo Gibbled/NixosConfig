be063d1d0f54d43fa39f40070b88a98f7fb6b362
Gibbled <lucksack@gmail.com>
2025-11-04 16:28:22 -0500

    formatting

--- modules/home-manager/sops.nix
+++ modules/home-manager/sops.nix
@@ -1,25 +1,21 @@
-{ config, inputs, pkgs, sops-nix, ... }:
-
 {
-
+  config,
+  inputs,
+  pkgs,
+  sops-nix,
+  ...
+}: {
   sops = {
     #age.keyFile = "/home/ranjit/.config/sops/age/keys.txt"; # must have no password!
 
     defaultSopsFile = ../../sops.yaml;
     validateSopsFiles = false;
-   #defaultSymlinkPath = "/run/user/1000/secrets";
+    #defaultSymlinkPath = "/run/user/1000/secrets";
     #defaultSecretsMountPoint = "/run/user/1000/secrets.d";
     age = {
-
-      sshKeyPaths = [ "/etc/ssh/host_key_ed25519_key" ];
+      sshKeyPaths = ["/etc/ssh/host_key_ed25519_key"];
       keyFile = "/var/lib/sops-nix/key";
       generateKey = true;
-
     };
-
-
   };
-
-
 }
-
